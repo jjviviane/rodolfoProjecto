fa3f8458b8707edde01e75f9494899c728ab4523
928681707jj <joaqui2314@gmail.com>
2024-03-22 14:25:47 +0000

    meuUltimoProjecto

---
+++ assets/JS/estilo.js
@@ -0,0 +1,19 @@
+ //#region estyle js for login 
+
+ const container= document.getElementById('container');
+ const registroBtn= document.getElementById('registro');
+ const loginBtn= document.getElementById('login');
+ 
+ 
+     registroBtn.addEventListener('click' , () =>
+     {
+       
+         container.classList.add('active');
+      
+     });
+     loginBtn.addEventListener('click' , () =>
+     {
+         container.classList.remove('active');
+     });
+ 
+      //#endregion 
